bce7eda098f36a32ca918ad3f0cc5c7d21d79ca2
SCSS77 <scastro@emagister.com>
2024-06-27 17:30:52 +0200

    Recipes website, first steps, add header footer and styles

---
+++ src/components/Footer.vue
@@ -0,0 +1,18 @@
+<template>
+  <footer class="footer">
+    <p class="footer_text">&copy; {{ currentYear }} Recipe App</p>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: 'Footer',
+  data() {
+    return {
+      currentYear: new Date().getFullYear()
+    };
+  }
+}
+</script>
+
+<style></style>
