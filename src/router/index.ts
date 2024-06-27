e1c1e460de2673b7c4cc9cefcdae044e48f57f4a
SCSS77 <scastro@emagister.com>
2024-06-27 18:22:47 +0200

    Recipes website, recipes detail page, router, components, styles

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,12 +3,14 @@ import HomeView from '@/views/HomeView.vue';
 import LoginView from "@/views/LoginView.vue";
 import FavoritesView from "@/views/FavoritesView.vue";
 import RecipesView from "@/views/RecipesView.vue";
+import RecipeDetailView from "@/views/RecipeDetailView.vue";
 
 const routes = [
     { path: '/', name: 'HomeView', component: HomeView },
     { path: '/login', name: 'Login', component: LoginView },
     { path: '/favorites', name: 'Favorites', component: FavoritesView },
-    { path: '/recipes', name: 'Recipes', component: RecipesView }
+    { path: '/recipes', name: 'Recipes', component: RecipesView },
+    { path: '/recipe/:id', name: 'RecipeDetail', component: RecipeDetailView, props: true }
 ];
 
 const router = createRouter({
---
+++ src/views/RecipeDetailView.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <RecipeDetail :id="recipeId" />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import RecipeDetail from '@/components/RecipeDetail.vue';
+import { useRoute } from 'vue-router';
+
+export default defineComponent({
+  name: 'RecipeDetailView',
+  components: {
+    RecipeDetail,
+  },
+  setup() {
+    const route = useRoute();
+    const recipeId = route.params.id;
+
+    return {
+      recipeId,
+    };
+  },
+});
+</script>
+
+<style></style>
