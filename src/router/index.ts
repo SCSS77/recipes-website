343312a66b0212f51b79b7f6df7b56f3d3eda9b1
SCSS77 <scastro@emagister.com>
2024-06-27 17:42:01 +0200

    Recipes website, login page, component, router, view, styles

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,10 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import HomeView from '@/views/HomeView.vue';
+import LoginView from "@/views/LoginView.vue";
 
 const routes = [
     { path: '/', name: 'HomeView', component: HomeView },
+    { path: '/login', name: 'Login', component: LoginView },
 ];
 
 const router = createRouter({
