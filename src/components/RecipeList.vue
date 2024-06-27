32643c5b9fdfaaf40e88983dcd9bb9b5389fbc95
SCSS77 <scastro@emagister.com>
2024-06-27 18:05:30 +0200

    Recipes website, recipes list page, router, components, styles

---
+++ src/components/RecipeList.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="recipes-section__container">
+    <div class="recipes-section__loader" v-if="loading">Loading recipes...</div>
+    <div class="recipes-section__no-found" v-else-if="recipes.length === 0">No recipes found.</div>
+    <div class="recipes-section__item" v-else v-for="recipe in recipes" :key="recipe.id">
+      <router-link :to="{ name: 'RecipeDetail', params: { id: recipe.id } }">
+        <h2 class="recipes-section__title">{{ recipe.title }}</h2>
+        <img class="recipes-section__image" :src="recipe.image" :alt="recipe.title" />
+      </router-link>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from 'vue';
+
+export default defineComponent({
+  name: 'RecipeList',
+  props: {
+    recipes: {
+      type: Array as PropType<Array<{ id: number; title: string; image: string }>>,
+      required: true,
+    },
+    loading: {
+      type: Boolean,
+      required: true,
+    },
+  },
+});
+</script>
+
+<style></style>
--- src/router/index.ts
+++ src/router/index.ts
@@ -2,11 +2,13 @@ import { createRouter, createWebHistory } from 'vue-router';
 import HomeView from '@/views/HomeView.vue';
 import LoginView from "@/views/LoginView.vue";
 import FavoritesView from "@/views/FavoritesView.vue";
+import RecipesView from "@/views/RecipesView.vue";
 
 const routes = [
     { path: '/', name: 'HomeView', component: HomeView },
     { path: '/login', name: 'Login', component: LoginView },
-    { path: '/favorites', name: 'Favorites', component: FavoritesView }
+    { path: '/favorites', name: 'Favorites', component: FavoritesView },
+    { path: '/recipes', name: 'Recipes', component: RecipesView }
 ];
 
 const router = createRouter({
